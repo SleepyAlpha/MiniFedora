03d0bb7755a7a080a7243b15e3d73ddc97a59b3d
Alexandre Almeida <53628036+SleepyAlpha@users.noreply.github.com>
2023-08-26 15:30:14 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
 echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf
 
 # Install gnome and utils
-sudo dnf install gnome-shell gnome-terminal nautilus gnome-software gnome-disk-utility xdg-user-dirs xdg-user-dirs-gtk git htop nano neofetch gamemode mangohud flatpak -y
+sudo dnf install gnome-shell gnome-terminal nautilus gnome-software gnome-disk-utility xdg-user-dirs xdg-user-dirs-gtk nano neofetch gamemode mangohud flatpak -y
 sudo systemctl enable gdm
 sudo systemctl set-default graphical.target
 
@@ -14,8 +14,8 @@ sudo systemctl set-default graphical.target
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak update
 
-# Install flatpak apps for laptop
-flatpak install flathub com.microsoft.Edge org.gnome.Geary org.gnome.Calendar org.libreoffice.LibreOffice org.gnome.eog org.gnome.Totem org.gnome.TextEditor org.gnome.seahorse.Application org.gnome.Calculator org.gnome.Maps org.gnome.Extensions com.valvesoftware.Steam com.heroicgameslauncher.hgl net.davidotek.pupgui2 tv.kodi.Kodi com.github.tchx84.Flatseal
+# Install flatpak apps
+flatpak install flathub com.microsoft.Edge org.gnome.Geary org.gnome.Calendar org.libreoffice.LibreOffice org.gnome.eog org.gnome.Totem org.gnome.TextEditor org.gnome.seahorse.Application org.gnome.Calculator org.gnome.Maps org.gnome.Extensions com.valvesoftware.Steam com.heroicgameslauncher.hgl tv.kodi.Kodi com.github.tchx84.Flatseal
 
 sudo dnf install fedora-workstation-repositories -y
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm -y
@@ -29,10 +29,10 @@ sudo dnf install dnf-plugins-core -y
 #sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y
 #sudo dnf install lame\* --exclude=lame-devel -y
 #sudo dnf group install --with-optional --allowerasing Multimedia -y
-sudo dnf install iwl7265-firmware
+sudo dnf install iwl7260-firmware -y
 
 # Install VM Tools
-sudo dnf group install virtualization
+# sudo dnf group install virtualization
 
 # Copy gamemode config
 sudo cp gamemode.ini /etc/
