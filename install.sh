54cdf6fa7826ba8d88414dce4d32c13f1d1b56c9
Alexandre Almeida <53628036+SleepyAlpha@users.noreply.github.com>
2023-08-26 03:14:15 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,14 +5,17 @@ echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf
 echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
 echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf
 
-# Install gnome with login manager and basic packages
-sudo dnf install gnome-shell gnome-terminal nautilus gnome-software gnome-disk-utility gnome-text-editor gnome-tweaks xdg-user-dirs xdg-user-dirs-gtk curl git htop nano neofetch gamemode flatpak -y
+# Install gnome and utils
+sudo dnf install gnome-shell gnome-terminal nautilus gnome-software gnome-disk-utility xdg-user-dirs xdg-user-dirs-gtk git htop nano neofetch gamemode mangohud flatpak -y
 sudo systemctl enable gdm
 sudo systemctl set-default graphical.target
 
-# Adding repos
+# Setup Flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak update
+
+# Install flatpak apps
+flatpak install flathub com.microsoft.Edge org.gnome.Geary org.gnome.Calendar org.libreoffice.LibreOffice org.gnome.eog org.gnome.Totem org.gnome.TextEditor org.gnome.seahorse.Application org.gnome.Calculator org.gnome.Maps org.gnome.Extensions com.valvesoftware.Steam com.heroicgameslauncher.hgl net.davidotek.pupgui2
 
 sudo dnf install fedora-workstation-repositories -y
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm -y
@@ -25,5 +28,4 @@ sudo dnf install dnf-plugins-core -y
 # Install media codecs
 sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y
 sudo dnf install lame\* --exclude=lame-devel -y
-sudo dnf group upgrade --with-optional --allowerasing Multimedia -y
-sudo dnf group upgrade --with-optional Multimedia -y
+sudo dnf group install --with-optional --allowerasing Multimedia -y
