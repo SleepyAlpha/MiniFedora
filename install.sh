134afb3e67df594319f0b5d45f2bc567a26330e1
Alexandre Almeida <53628036+SleepyAlpha@users.noreply.github.com>
2023-08-26 14:39:57 +0100

    test

--- install.sh
+++ install.sh
@@ -14,8 +14,8 @@ sudo systemctl set-default graphical.target
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak update
 
-# Install flatpak apps
-flatpak install flathub com.microsoft.Edge org.gnome.Geary org.gnome.Calendar org.libreoffice.LibreOffice org.gnome.eog org.gnome.Totem org.gnome.TextEditor org.gnome.seahorse.Application org.gnome.Calculator org.gnome.Maps org.gnome.Extensions com.valvesoftware.Steam com.heroicgameslauncher.hgl net.davidotek.pupgui2
+# Install flatpak apps for laptop
+flatpak install flathub com.microsoft.Edge org.gnome.Geary org.gnome.Calendar org.libreoffice.LibreOffice org.gnome.eog org.gnome.Totem org.gnome.TextEditor org.gnome.seahorse.Application org.gnome.Calculator org.gnome.Maps org.gnome.Extensions com.valvesoftware.Steam com.heroicgameslauncher.hgl net.davidotek.pupgui2 tv.kodi.Kodi com.github.tchx84.Flatseal
 
 sudo dnf install fedora-workstation-repositories -y
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm -y
@@ -26,9 +26,13 @@ sudo dnf install rpmfusion-free-release-tainted -y
 sudo dnf install dnf-plugins-core -y
 
 # Install media codecs
-sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y
-sudo dnf install lame\* --exclude=lame-devel -y
-sudo dnf group install --with-optional --allowerasing Multimedia -y
+#sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y
+#sudo dnf install lame\* --exclude=lame-devel -y
+#sudo dnf group install --with-optional --allowerasing Multimedia -y
+sudo dnf install iwl7265-firmware
+
+# Install VM Tools
+sudo dnf group install virtualization
 
 # Copy gamemode config
 sudo cp gamemode.ini /etc/
