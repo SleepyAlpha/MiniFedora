51ce873e75dbb51c1a5e14c4bd2320cc0c96468f
Alexandre Almeida <53628036+SleepyAlpha@users.noreply.github.com>
2023-08-29 10:07:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,38 +1,36 @@
-# Original script at https://codeberg.org/emylfy/fedora-minimal
+# Speedup DNF
 
-# Dnf flags
 echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf
 echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
 echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf
 
-# Install gnome and utils
-sudo dnf install gnome-shell gnome-terminal nautilus gnome-software gnome-disk-utility xdg-user-dirs xdg-user-dirs-gtk nano neofetch gamemode mangohud flatpak timeshift -y
+# Install Gnome and Base Utils
+
+sudo dnf install gnome-shell nautilus gnome-software gnome-console xdg-user-dirs xdg-user-dirs-gtk neofetch gamemode mangohud flatpak timeshift -y
 sudo systemctl enable gdm
 sudo systemctl set-default graphical.target
 
-# Setup Flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak update
+# Install Wifi Firmware For Laptop
 
-# Install flatpak apps
-flatpak install flathub com.microsoft.Edge org.gnome.Geary org.gnome.Calendar org.libreoffice.LibreOffice org.gnome.eog org.gnome.Totem org.gnome.TextEditor org.gnome.seahorse.Application org.gnome.Calculator org.gnome.Maps org.gnome.Extensions com.valvesoftware.Steam com.heroicgameslauncher.hgl tv.kodi.Kodi com.github.tchx84.Flatseal
-
-sudo dnf install fedora-workstation-repositories -y
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm -y
-sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm -y
-sudo dnf upgrade --refresh -y
-sudo dnf groupupdate core -y
-sudo dnf install rpmfusion-free-release-tainted -y
-sudo dnf install dnf-plugins-core -y
-
-# Install media codecs
-#sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y
-#sudo dnf install lame\* --exclude=lame-devel -y
-#sudo dnf group install --with-optional --allowerasing Multimedia -y
 sudo dnf install iwl7260-firmware -y
 
-# Install VM Tools
-# sudo dnf group install virtualization
+# Install VM Utils
 
-# Copy gamemode config
+sudo dnf group install virtualization -y
+
+# Install FlatHub
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak update -y
+
+# Install Core Flatpaks
+
+flatpak install flathub com.microsoft.Edge org.libreoffice.LibreOffice org.gnome.Evince org.gnome.Evolution org.gtk.Gtk3theme.Adwaita-dark org.gnome.eog org.gnome.Totem org.gnome.TextEditor org.gnome.Calculator com.github.tchx84.Flatseal -y
+
+#Install Optional Flatpaks
+
+flatpak install flathub com.valvesoftware.Steam com.heroicgameslauncher.hgl tv.kodi.Kodi -y
+
+# Copy Gamemode Config
+
 sudo cp gamemode.ini /etc/
